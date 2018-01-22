5fe4bfbfc60f0fb315ee3665a0ca2255d6421d92
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-22 21:49:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   def self.new_by_filename(file_name)
     parts = []
     parts = file_name.split(" - ")
-    song = Song.new(parts[1])
+    song = self.new(parts[1])
     song.artist_name = parts[0]
     #song.name = parts[1]
     song
